ab4483b235d115c51c895fcea394ab270d47e645
PIETTE Quentin <q.piette@rubika-edu.com>
2025-11-04 16:31:34 +0100

    deplacement perso + tilemap

---
+++ Game/Player/player.gd
@@ -0,0 +1,25 @@
+class_name Player extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+@export var camera: Camera2D
+@export var max_health: int
+
+
+func _physics_process(delta: float) -> void:
+
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
