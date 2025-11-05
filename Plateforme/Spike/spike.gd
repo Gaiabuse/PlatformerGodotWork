881ec7b1f00bc64dd4cbe0160a22359d86914a16
COMBAULT Lazare <l.combault@rubika-edu.com>
2025-11-05 14:24:02 +0100

    add laser weapon and start inventory

--- Plateforme/Spike/spike.gd
+++ Plateforme/Spike/spike.gd
@@ -1,26 +1,35 @@
-extends StaticBody2D
+class_name Spike extends StaticBody2D
 @export_group("references")
-@export var animation : AnimationPlayer
 @export var spike_out_timer : Timer
 @export var spike_in_timer : Timer
+@export var cooldown : Timer
 @export_group("settings")
-@export var height : float
+@export var height : float = -30
 @export var speed : float = 200
+@export var strength : int =1
+@export var spike_out_wait_time : float = 0.6
+@export var spike_in_wait_time : float = 2
+@export var is_movable : bool = true
+
 var moving : bool
 var destination : float
 var startPosition : float
 var returnToStart: bool
 var _start_spike_system : bool
+var _timer_start : bool
 var move : Vector2 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	returnToStart = false
+	_initialize()
 	spike_in_timer.start()
+	spike_out_timer.wait_time = spike_out_wait_time
+	spike_in_timer.wait_time = spike_in_wait_time
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if _start_spike_system :
+	if _start_spike_system and is_movable:
 		spike_moving()
 		global_position = global_position.move_toward(move, delta*speed)
 	
@@ -32,7 +41,8 @@ func spike_moving():
 			move = Vector2(global_position.x,destination)
 		returnToStart = !returnToStart
 		moving = false
-	if global_position == move:
+	if global_position == move and !_timer_start:
+		_timer_start = true
 		if(returnToStart):
 			spike_out_timer.start()
 		else :
@@ -45,10 +55,12 @@ func _initialize()-> void:
 
 
 func _on_spike_out_timer_timeout() -> void:
+	_timer_start = false
 	_start_spike_system = true
 	moving = true
 
 
 func _on_spike_in_timer_timeout() -> void:
+	_timer_start = false
 	_start_spike_system = true
 	moving = true
