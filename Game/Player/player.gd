04a6fc6ea0d68c206b95fc30d24ddc66076b891a
PIETTE Quentin <q.piette@rubika-edu.com>
2025-11-06 17:28:04 +0100

    son et musique jeu

--- Game/Player/player.gd
+++ Game/Player/player.gd
@@ -43,6 +43,7 @@ func _physics_process(delta: float) -> void:
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$JumpPlayer.play()
 
 	var direction: float = Input.get_axis("move_left", "move_right")
 	velocity.x = direction * SPEED
