db9982391dff180f23062fd30be1885550c170e7
Hippolyte <jombarthippolyte@gmail.com>
2025-11-06 15:03:04 +0100

    animation mort ennemie

--- Game/ennemies/basic_ennemy/enemy.gd
+++ Game/ennemies/basic_ennemy/enemy.gd
@@ -21,22 +21,11 @@ var direction: Vector2 = Vector2.ZERO
 var target_direction: Vector2 = Vector2.ZERO
 var target_position: Vector2 = Vector2.ZERO
 
-#Gestion des damages sur le joueur
-@export var timer : Timer
-var _is_attack: bool = false
-var attacking: Player
-
 
 func _ready() -> void:
 	_health = max_health
 	limiteGauche = position.x - limiteGauche
 	limiteDroite += position.x
-
-
-func _process(delta: float) -> void:
-	#Attaque
-	if attacking:
-		make_damage(attacking)
 
 func _physics_process(delta: float) -> void:
 	
@@ -79,23 +68,6 @@ func _on_sight_area_body_exited(body: Node2D) -> void:
 	if body is Player:
 		player = null
 
-'''Gestion des dégâts aux joueur'''
-func _on_collision_area_body_entered(body: Node2D) -> void:
-	if body is Player:
-		attacking = body
-
-func _on_collision_area_body_exited(body: Node2D) -> void:
-	attacking = null
-
-func make_damage(body: Node2D) -> void:
-	if !_is_attack:
-		body.take_damage(1)
-		timer.start()
-		_is_attack = true
-
-func _on_timer_timeout() -> void:
-	_is_attack = false
-
 '''Gestion de la vie'''
 func take_damage(damage:int):
 	_health = max(0, _health - damage)
@@ -105,5 +77,8 @@ func take_damage(damage:int):
 func die():
 	if _is_dead:
 		return
+	$animation.play("died")
+	set_collision_layer_value(4, false)
 	_is_dead = true
+	await  $animation.animation_finished
 	queue_free()
