d420b859b063c99efb979f5f32435464a7f0a06a
COMBAULT Lazare <l.combault@rubika-edu.com>
2025-11-06 17:13:00 +0100

    je test

--- Game/checkpoint/checkpoint.gd
+++ Game/checkpoint/checkpoint.gd
@@ -5,6 +5,7 @@ extends Node2D
 func _on_body_entered(body: Node2D) -> void:
 	CheckpointVar.checkpoint = global_position
 	$".".set_deferred("monitoring",false)
+	$CheckpointSound.play()
 	print(CheckpointVar.checkpoint)
 	
 	var explosionVFX:Node2D = explosion_scene.instantiate()
