c1c35a60286baef2585e89272bf1045053a4994c
Liam <ltconflans@gmail.com>
2025-11-06 15:40:18 +0100

    texture checkpoint + fix checkpoints

--- Game/checkpoint/checkpoint.gd
+++ Game/checkpoint/checkpoint.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
-
 func _on_body_entered(body: Node2D) -> void:
 	CheckpointVar.checkpoint = global_position
-	queue_free()
+	$".".set_deferred("monitoring",false)
 	print(CheckpointVar.checkpoint)
