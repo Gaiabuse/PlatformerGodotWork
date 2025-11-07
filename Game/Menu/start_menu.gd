1fb5790c94961893fa13dadea6667e60dbc79364
Liam <ltconflans@gmail.com>
2025-11-07 16:14:12 +0100

    kaka en vrai jsp

--- Game/Menu/start_menu.gd
+++ Game/Menu/start_menu.gd
@@ -3,6 +3,7 @@ extends Control
 
 func _on_start_button_button_up() -> void:
 	get_tree().change_scene_to_file(level_1)
+	CheckpointVar.checkpoint = Vector2(588,343)
 
 
 func _on_quit_button_button_up() -> void:
